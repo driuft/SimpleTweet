ba8e4035d054e6195fbc3db451390d023d26ffca
Aldo Socarras <air.socarras@gmail.com>
2021-02-07 02:07:49 -0500

    Added functionality to refresh tweets timeline by using the SwipeRefreshLayout (pull-to-refresh)

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -44,8 +44,18 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
     }
 
     @Override
-    public int getItemCount() {
-        return tweets.size();
+    public int getItemCount() { return tweets.size(); }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items
+    public void addAll(List<Tweet> tweetList) {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
     }
 
     // Define a viewholder
